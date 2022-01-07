9d705ba53536b50fa130e15adef5e8d3206d16cc
蔡聪昊 <caiconghao@icloud.com>
2022-01-07 23:12:53 +0800

    feat: moveList action creator

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -2,6 +2,16 @@ type Item = {
   id: string;
 };
 
+type MoveItem = <TItem>(array: TItem[], from: number, to: number) => TItem[];
+
+type RemoveItemAtIndex = <TItem>(array: TItem[], index: number) => TItem[];
+
+type InsertItemAtIndex = <TItem>(
+  array: TItem[],
+  item: TItem,
+  index: number
+) => TItem[];
+
 export const findItemIndexById = <TItem extends Item>(
   items: TItem[],
   id: string
@@ -16,19 +26,15 @@ export const findItemIndexById = <TItem extends Item>(
  * @param to ending index
  * @returns new array which is been updated by moving its item
  */
-export const moveItem = <TItem>(array: TItem[], from: number, to: number) => {
+export const moveItem: MoveItem = (array, from, to) => {
   const item = array[from];
   return insertItemAtIndex(removeItemAtIndex(array, from), item, to);
 };
 
-export const removeItemAtIndex = <TItem>(array: TItem[], index: number) => {
+export const removeItemAtIndex: RemoveItemAtIndex = (array, index) => {
   return [...array.slice(0, index), ...array.slice(index + 1)];
 };
 
-export const insertItemAtIndex = <TItem>(
-  array: TItem[],
-  item: TItem,
-  index: number
-) => {
+export const insertItemAtIndex: InsertItemAtIndex = (array, item, index) => {
   return [...array.slice(0, index), item, ...array.slice(index)];
 };
